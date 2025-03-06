62d087f0a544d997427e5f7b28d2e87aa542e340
rahmed <rania.k.zaghloul@gmail.com>
2025-03-06 14:39:53 +0200

    Second Project Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Trello Link 
+https://trello.com/b/Fe9Hw23l/udacity-devops
+
+# Spreadsheet link
+https://docs.google.com/spreadsheets/d/1-k0SX3xOwua-6DuVwF5SqJBEC8fUNi6mpXoaCFjnyCk/edit?usp=sharing
---
+++ make_predict_azure_app.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+PORT=443
+echo "Port: $PORT"
+
+# POST method predict
+curl -d '{
+   "CHAS":{
+      "0":0
+   },
+   "RM":{
+      "0":6.575
+   },
+   "TAX":{
+      "0":296.0
+   },
+   "PTRATIO":{
+      "0":15.3
+   },
+   "B":{
+      "0":396.9
+   },
+   "LSTAT":{
+      "0":4.98
+   }
+}'\
+     -H "Content-Type: application/json" \
+     -X POST https://witty-desert-23a17c7f08f146a49c37239bb72780b4.azurewebsites.net:$PORT/predict 
+     #your application name <yourappname>goes here
